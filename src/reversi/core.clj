4ce49e6ab06aecba2b95be90e5f38c6b15c9b3ab
Martin Forsgren <martin.forsgren@gmail.com>
2012-11-13 22:46:39 +0100

    Defined board, *board-size* and print-board.

--- src/reversi/core.clj
+++ src/reversi/core.clj
@@ -1,6 +1,20 @@
 (ns reversi.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+;; TODO: tree, minimax, alphabeta, heuristics
+;; tree: legal moves: inside bounds, and flips at least a piece
+
+(def *board-size* 8)
+
+(def board {[3 3] \b [4 3] \w
+            [3 4] \w [4 4] \b})
+
+(defn print-board [board]
+  (let [line (apply str (repeat (* 2 *board-size*) \-))]
+    (println line)
+    (doseq [y (range *board-size*)
+            x (range *board-size*)]
+      (print (or (board [x y]) \space))
+      (print \|)
+      (when (= x 7)
+        (println)
+        (println line)))))
