ce5eddbdac21dd04ad4844dd7960750d9e368c3c
Martin Forsgren <martin.forsgren@gmail.com>
2012-12-04 14:22:04 +0100

    Add str->move and move->str, update main to actually return a move.

--- src/reversi/main.clj
+++ src/reversi/main.clj
@@ -12,8 +12,25 @@ other characters represents the board and should be either E for empty,
 O for white or X for black.
 "))
 
-(defn -main [position time-limit & args]
-  (if-not (and position time-limit (zero? (count args)))
+(defn str->move [[str-player &  str-board]]
+  (let [player ({\W \w, \B \b} str-player)
+        board (into {} (remove (comp nil? second)
+                               (zipmap (for [y (range *board-size*)
+                                             x (range *board-size*)]
+                                         [x y])
+                                       (map {\E nil, \O \w, \X \b} str-board))))]
+    [board player]))
+
+(defn move->str [[prev-board p1] [board _]]
+  (let [[[x y]] (seq (intersection (set (legal-positions [prev-board p1]))
+                                   (difference (set (keys board)) (set (keys prev-board)))))]
+    (str "(" y "," x ")")))
+
+(defn -main [move-str time-limit & args]
+  (if-not (and move-str time-limit (zero? (count args)))
     (usage)
-    nil ; TODO do stuff
-    ))
+    (let [move (str->move move-str)
+          next-move (first (ai-player heur/position 7 (game-tree move)))]
+      (move->str move next-move))))
+
+"WEEEEEEEEEEEEEEEEEEEEEEEEEEEOXEEEEEEXOEEEEEEEEEEEEEEEEEEEEEEEEEEE"
